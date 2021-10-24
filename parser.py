f76026b8615026f8f22d876f1eaf123ca22b58ed
neilblaze <putubanerjee23@gmail.com>
2021-10-24 19:09:00 +0530

    ./rm rf frontend

--- parser.py
+++
@@ -1,9 +0,0 @@
-l = list()
-with open("/Users/irennanicole/Desktop/archive/Symptom-severity.csv") as f:
-   content = f.read()
-   content = content.split("\n")[1:-1]
-   for c in content:
-      c = c.split(",")
-      l.append(c[0].replace("_", " "))
-
-print(l)
